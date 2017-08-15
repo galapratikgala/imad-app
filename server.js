0e62b5a0f8feeab3ca382f216cfd0f4ead79f71c
galapratikgala <30749319+galapratikgala@users.noreply.github.com>
2017-08-15 15:44:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article-one', function (req, res) {
  res.send('Article one will be requested here');
 }); 
 app.get('/article-two', function (req, res) {
- res.send('Article one will be requested here');
+ res.send('Article two will be requested here');
 }); 
 app.get('/article-three', function (req, res) {
- res.send('Article one will be requested here');
+ res.send('Article three will be requested here');
 }); 
 
 app.get('/ui/style.css', function (req, res) {
